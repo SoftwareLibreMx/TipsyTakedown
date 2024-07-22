3299e6ea5361f8e5a1862d84bc18df6426397993
Balam Palma <balam.palma@interfactura.com>
2024-07-21 21:27:15 -0600

    moving the routes and config to a better place

--- www/app.py
+++ www/app.py
@@ -1,8 +1,10 @@
 import flask
-from flask import redirect, url_for, session
+# from authlib.integrations.flask_client import OAuth
 from webapp import webapp
 from api.shared.infraestructure.controller import api
-from authlib.integrations.flask_client import OAuth
+
+# # Initialize OAuth
+# oauth = OAuth()
 
 def create_app():
     flask_app = flask.Flask(
@@ -10,65 +12,26 @@ def create_app():
         template_folder='webapp/templates',
         static_folder='webapp/static')
 
-    # Initialize OAuth
-    oauth = OAuth(flask_app)
-    # Configure the Google OAuth client
-    google = oauth.register(
-        name='google',
-        client_id='YOUR_CLIENT_ID',
-        client_secret='YOUR_CLIENT_SECRET',
-        authorize_url='https://accounts.google.com/o/oauth2/auth',
-        # authorize_params=None,
-        access_token_url='https://accounts.google.com/o/oauth2/token',
-        # access_token_params=None,
-        # refresh_token_url=None,
-        redirect_uri='http://localhost:5000/auth',
-        client_kwargs={'scope': 'openid profile email'}
-    )
-    # Configure the Facebook OAuth client
-    facebook = oauth.register(
-        name='facebook',
-        client_id='YOUR_CLIENT_ID',
-        client_secret='YOUR_CLIENT_SECRET',
-        authorize_url='https://www.facebook.com/dialog/oauth',
-        # authorize_params=None,
-        access_token_url='https://graph.facebook.com/oauth/access_token',
-        # access_token_params=None,
-        # refresh_token_url=None,
-        redirect_uri='http://localhost:5000/auth/facebook',
-        client_kwargs={'scope': 'email'}
-    )
+    # flask_app.config.from_mapping(
+    #     SECRET_KEY='your_secret_key',
+    #     OAUTH_CREDENTIALS={
+    #         'google': {
+    #             'client_id': 'your_google_client_id',
+    #             'client_secret': 'your_google_client_secret',
+    #         },
+    #         'facebook': {
+    #             'client_id': 'your_facebook_client_id',
+    #             'client_secret': 'your_facebook_client_secret',
+    #         }
+    #     }
+    # )
+
+    # oauth.init_app(flask_app)
+
 
     @flask_app.route('/health')
     def health():
         return 'OK'
-
-    # GOOGLE LOGIN ROUTES 
-    @flask_app.route('/login/google')
-    def login_google():
-        redirect_uri = url_for('auth', _external=True)
-        return google.authorize_redirect(redirect_uri)
-
-    @flask_app.route('/auth/google')
-    def auth_google():
-        token = google.authorize_access_token()
-        user_info = google.parse_id_token(token)
-        session['email'] = user_info['email']
-        return redirect('/')
-
-    # FACEBOOK LOGIN ROUTES
-    @flask_app.route('/login/facebook')
-    def login_facebook():
-        redirect_uri = url_for('auth_facebook', _external=True)
-        return facebook.authorize_redirect(redirect_uri)
-
-    @flask_app.route('/auth/facebook')
-    def auth_facebook():
-        token = facebook.authorize_access_token()
-        resp = facebook.get('https://graph.facebook.com/me?fields=id,name,email', token=token)
-        user_info = resp.json()
-        session['email'] = user_info.get('email')
-        return redirect('/')
 
     flask_app.register_blueprint(webapp)
     flask_app.register_blueprint(api, url_prefix='/api/')
--- www/webapp/__init__.py
+++ www/webapp/__init__.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, session
+# from authlib.integrations.flask_client import OAuth
+# from app import oauth
 
 from .modules.materials import materials_router
 
@@ -7,6 +9,63 @@ webapp = Blueprint('web', __name__)
 webapp.register_blueprint(materials_router, url_prefix='/material')
 
 
+# # Configure the Google OAuth client
+# google = oauth.register(
+#     name='google',
+#     client_id='YOUR_CLIENT_ID',
+#     client_secret='YOUR_CLIENT_SECRET',
+#     authorize_url='https://accounts.google.com/o/oauth2/auth',
+#     # authorize_params=None,
+#     access_token_url='https://accounts.google.com/o/oauth2/token',
+#     # access_token_params=None,
+#     # refresh_token_url=None,
+#     redirect_uri='http://localhost:8000/auth/google',
+#     client_kwargs={'scope': 'openid profile email'}
+# )
+# # Configure the Facebook OAuth client
+# facebook = oauth.register(
+#     name='facebook',
+#     client_id='YOUR_CLIENT_ID',
+#     client_secret='YOUR_CLIENT_SECRET',
+#     authorize_url='https://www.facebook.com/dialog/oauth',
+#     # authorize_params=None,
+#     access_token_url='https://graph.facebook.com/oauth/access_token',
+#     # access_token_params=None,
+#     # refresh_token_url=None,
+#     redirect_uri='http://localhost:8000/auth/facebook',
+#     client_kwargs={'scope': 'email'}
+# )
+
 @webapp.route('/')
 def index():
     return render_template("index.html")
+
+@webapp.route('/login')
+def login():
+    return render_template("login.html")
+
+# # GOOGLE LOGIN ROUTES 
+# @webapp.route('/login/google')
+# def login_google():
+#     redirect_uri = url_for('auth.authorize', _external=True)
+#     return google.authorize_redirect(redirect_uri)
+
+# @webapp.route('/auth/google')
+# def auth_google():
+#     token = google.authorize_access_token()
+#     user_info = google.parse_id_token(token)
+#     session['user'] = user_info
+#     return redirect('/')
+
+# # FACEBOOK LOGIN ROUTES
+# @webapp.route('/login/facebook')
+# def login_facebook():
+#     redirect_uri = url_for('auth.facebook_authorize', _external=True)
+#     return facebook.authorize_redirect(redirect_uri)
+
+# @webapp.route('/auth/facebook')
+# def auth_facebook():
+#     token = facebook.authorize_access_token()
+#     user_info = facebook.get('https://graph.facebook.com/me?fields=id,name,email', token=token).json()
+#     session['user'] = user_info
+#     return redirect('/')
