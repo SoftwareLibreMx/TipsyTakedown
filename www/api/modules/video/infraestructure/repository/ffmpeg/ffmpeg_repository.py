0df8968ae24cb54d6cf25c6e52355a22e3dd46d7
Alfonso Villalobos <alfonsocvu@gmail.com>
2024-07-24 22:56:42 -0600

    fix: sonartest indent

--- www/api/modules/video/infraestructure/repository/ffmpeg/ffmpeg_repository.py
+++ www/api/modules/video/infraestructure/repository/ffmpeg/ffmpeg_repository.py
@@ -5,8 +5,8 @@ class FFMPEGRepository:
     location_prefix = './tmp'
 
     def encode(self, scale: str, file_name: str) -> str:
-        file_output_name = f'{
-            self.location_prefix}/{file_name}_output_{scale}.mp4'
+        output_f_name = f'{file_name}_output_{scale}.mp4'
+        file_output_name = f'{self.location_prefix}/{output_f_name}'
         resp = subprocess.run(['ffmpeg', '-y',
                                '-i', f'{self.location_prefix}/{file_name}',
                                '-vf', f'scale={scale}:-1',
