60f313f5ee4b64282b91478a314c965521db0f5b
Balam Palma <balam.palma@interfactura.com>
2024-07-29 21:09:18 -0600

    commenting code for merge the other components

--- www/api/libs/utils/authorize/authorize.py
+++ www/api/libs/utils/authorize/authorize.py
@@ -5,18 +5,18 @@ from api.libs.domain_entity import UserType
 def authorize(user_type_required: UserType):
 
 # @authorize(UserType.ADMIN)from functools import wraps
+	return None
+	# def login_required(f):
+	# 	@wraps(f)
+	# 	def decorated_function(*args, **kwargs):
+	# 		# add validation of the token
+	# 		authorization_token = request.headers.get('Auhtorization', None)
+	# 		# jwt TODO IMPLEMENT JWT 
 
-	def login_required(f):
-		@wraps(f)
-		def decorated_function(*args, **kwargs):
-			# add validation of the token
-			authorization_token = request.headers.get('Auhtorization', None)
-			# jwt TODO IMPLEMENT JWT 
-
-			# if 
-			if g.user is None:
-				return redirect(url_for('login', next=request.url))
-			return f(*args, **kwargs)
-		return decorated_function
+	# 		# if 
+	# 		if g.user is None:
+	# 			return redirect(url_for('login', next=request.url))
+	# 		return f(*args, **kwargs)
+	# 	return decorated_function
 	
-	return login_required
+	# return login_required
