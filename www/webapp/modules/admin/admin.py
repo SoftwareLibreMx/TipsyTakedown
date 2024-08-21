b79b49e531b19c10a7aa54903a3292f6afda276e
Alfonso Villalobos <alfonsocvu@gmail.com>
2024-08-21 09:49:06 -0600

    chore: remove not use temples restructure admin templates

---
+++ www/webapp/modules/admin/admin.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, render_template
+
+TEMPLATE_DIR = 'admin'
+admin_router = Blueprint('admin', __name__)
+
+
+@admin_router.route('/video/uploader')
+def upload_video():
+    return render_template(f'{TEMPLATE_DIR}/video/uploader/index.html')
